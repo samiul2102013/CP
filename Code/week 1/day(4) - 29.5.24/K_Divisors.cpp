626bda187de9cee15109b0c58911cebabe18e9f5
samiul2102013 <samiulhasan0186@gmail.com>
2024-05-30 18:46:17 +0600

    day 4

---
+++ Code/week 1/day(4) - 29.5.24/K_Divisors.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int N = 2e5 +5;
+typedef pair<int,int> pii;
+
+
+
+void TEST_CASES()
+{
+    int n;cin>>n;
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            cout<<i<<endl;
+        }
+    }
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t=1;
+    //cin>>t;
+    while(t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
